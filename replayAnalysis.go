925791cecff71f848ac0b6185436e6f9b1cf0171
Zaryab Farooq <zfarooq@ucdavis.edu>
2018-05-17 15:18:26 -0700

    Create replayAnalysis.go

---
+++ replayAnalysis.go
@@ -0,0 +1,40 @@
+/*
+This example shows how to use the rep package to easily extract information
+from a StarCraft II (*.SC2Replay) file.
+*/
+package main
+
+import (
+	"fmt"
+
+	"github.com/icza/s2prot/rep"
+)
+
+func main() {
+	//r, err := rep.NewFromFileEvts("../../mpq/reps/automm.SC2Replay", true, true, true)
+	r, err := rep.NewFromFile("king_sejong_station_le.sc2replay")
+	if err != nil {
+		fmt.Printf("Failed to open file: %v\n", err)
+		return
+	}
+	defer r.Close()
+
+	fmt.Printf("Version:        %v\n", r.Header.VersionString())
+	fmt.Printf("Loops:          %d\n", r.Header.Loops())
+	fmt.Printf("Length:         %v\n", r.Header.Duration())
+	fmt.Printf("Map:            %s\n", r.Details.Title())
+	fmt.Printf("Game events:    %d\n", len(r.GameEvts))
+	fmt.Printf("Message events: %d\n", len(r.MessageEvts))
+	fmt.Printf("Tracker events: %d\n", len(r.TrackerEvts.Evts))
+
+	fmt.Println("Players:")
+	for _, p := range r.Details.Players() {
+		fmt.Printf("\tName: %-20s, Race: %c, Team: %d, Result: %v\n",
+			p.Name, p.Race().Letter, p.TeamID()+1, p.Result())
+	}
+	fmt.Printf("Full Header:\n%v\n", r.Header)
+
+	//fmt.Printf("%s\n", r.Details.String())
+	//fmt.Printf("%s\n", r.InitData.String())
+	//fmt.Printf("%s\n", r.AttrEvts.String())
+}
