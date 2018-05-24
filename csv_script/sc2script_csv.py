b26e6898a8b9838e6549301d5e21d2ee4029dc40
chaare24 <38058323+chaare24@users.noreply.github.com>
2018-05-24 14:33:29 -0700

    Parses Data

--- csv_script/sc2script_csv.py
+++ csv_script/sc2script_csv.py
@@ -4,6 +4,52 @@ import sys
 from os.path import isfile, join
 import sc2reader
 from sc2reader.engine.plugins import SelectionTracker, APMTracker
+def csv_out(results):
+    for i in range(0,len(results)):
+
+        print('unit_type: ' + results[i]['unit_type:'] + ", "
+              'unit_id: ' + results[i]['unit_id:'] + ", "
+              'purchase_game_second: ' + results[i]['purchase_game_second:'] + ", "
+              'purchase_x_coordinate: ' + results[i]['purchase_x_coordinate:'] + ", "
+              'purchase_y_coordinate: ' + results[i]['purchase_y_coordinate:'] + ", "
+              'death_game_second: ' + results[i]['death_game_second:'] + ", "
+              'death_x_coordinate: ' + results[i]['death_x_coordinate:'] + ", "
+              'death_y_coordinate: ' + results[i]['death_y_coordinate:'] + ", "
+              'killing_unit_id: ' + results[i]['killing_unit_id:'] + ", "
+              'life_span: ' + results[i]['life_span:']
+        )
+
+
+
+
+def getData(created, deaths):
+    results = []
+    for i in range(0,len(created)):
+        for j in range(0, len(deaths)):
+            if created[i]['unit_id:'] == deaths[j]['unit_id:']:
+                survival = deaths[j]['game_second:'] - created[i]['game_second:']
+
+                game_results = {
+                    'life_span:': str(survival),
+
+                    'killing_unit_id:': str(deaths[j]['killing_unit_id:']),
+                    'death_x_coordinate:': str(deaths[j]['x_coordinate:']),
+                    'death_y_coordinate:': str(deaths[j]['y_coordinate:']),
+                    'death_game_second:': str(deaths[j]['game_second:']),
+
+
+
+                    'purchase_x_coordinate:': str(created[i]['x_coordinate:']),
+                    'purchase_y_coordinate:': str(created[i]['y_coordinate:']),
+                    'purchase_game_second:': str(created[i]['game_second:']),
+                    'unit_id:': str(created[i]['unit_id:']),
+                    'unit_type:': str(created[i]['unit_type:'])
+                }
+                # print(game_results)
+                results.append(game_results)
+    #print(results)
+    csv_out(results)
+
 
 def sc2_parser():
     parser = argparse.ArgumentParser()
@@ -24,30 +70,26 @@ def extract(in_path, out_path):
                     'x_coordinate:': event.location[0],
                     'y_coordinate:': event.location[1],
                     'unit_type:': event.unit_type_name,
-                    'game_second': event.second,
+                    'game_second:': event.second,
                     #'player': event.unit_controller.toon_id,
-                    'unit_id': event.unit_id
+                    'unit_id:': event.unit_id
                 }
 
                 created.append(created_data)
 
         if type(event) is sc2reader.events.tracker.UnitDiedEvent:
             death_data = {
-                'killing_unit_id': event.killing_unit_id,
+                'killing_unit_id:': event.killing_unit_id,
                 'x_coordinate:': event.location[0],
                 'y_coordinate:': event.location[1],
-                'game_second': event.second,
+                'game_second:': event.second,
                 #'unit_type:': event.unit_type_name,                    'game_second': event.second,
                 #'player': event.unit_controller.toon_id,
-                'unit_id': event.unit_id
+                'unit_id:': event.unit_id
             }
 
             deaths.append(death_data)
-
-    print(deaths)
-
-
-
+    getData(created, deaths)
 
 
 def main():
